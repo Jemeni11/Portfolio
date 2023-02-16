92123765aecdb1ca13ffdb184f174861e39b6058
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-02-16 16:12:54 +0100

    feat(pages): created several new pages and added a herosection to the home page
    BREAKING CHANGE:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { PT_Sans } from "@next/font/google";
+import Navbar from "@/components/Navbar";
+
+const PTSans = PT_Sans({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={PTSans.className}>
+      <Navbar />
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ pages/tools.tsx
@@ -0,0 +1,9 @@
+const Tools = () => {
+  return (
+    <div>
+      <h1>Tools</h1>
+    </div>
+  );
+};
+
+export default Tools;
