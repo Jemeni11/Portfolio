a80a1f1ddc5ca267da13b4f942ffa9a4c0aa0361
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-06 17:46:00 +0100

    Deleted  and refactored

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import styles from "@/styles/About.module.css";
+import Quote from "@/components/Quote";
+import KaushanScriptText from "@/components/KaushanScriptText";
 
 const About = () => {
   return (
@@ -11,10 +12,22 @@ const About = () => {
         />
         <title>About | Emmanuel Jemeni</title>
       </Head>
-      <div className={styles.aboutPageContainer}>
+      <div className="w-full flex flex-col min-h-screen">
+        <section className="w-full min-h-[85vh] flex flex-col justify-center items-center gap-y-2">
+          <p className=" font-bold text-[10rem] text-white/[.725]">
+            <KaushanScriptText text="Hi There" />
+          </p>
+        </section>
         <section>
-          <p>yo</p>
-          <h1>I'm Emmanuel !</h1>
+          <Quote
+            cite_link="https://me.me/i/jesus-apolinaris-we-are-not-strangers-only-the-introduction-is-quote-8c1ebf3c825643b5b6160e655063c961"
+            quote="We are not strangers, only the introduction is missing."
+            author="Jesus Apolinaris"
+          />
+        </section>
+        <section>
+          <h1>I'm Emmanuel Chinonso Jemeni</h1>
+          <KaushanScriptText text="Pronunciation: ih-MAH-nyu-wuhl CHEE-none-so GEM-many" />
         </section>
       </div>
     </>
