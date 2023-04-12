65e5285cd8859d22689e6b9e1e40ebb25b67be9e
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-12 17:25:30 +0100

    feat(contact.tsx): Imported the **PageTemplate** component, removed the head import, added a quote, imported contact data from the data folder and completed the page
    BREAKING CHANGE:

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,53 @@
-import Head from "next/head";
+import PageTemplate from "@/components/PageTemplate";
+import Quote from "@/components/Quote";
+import { contacts } from "@/data/contact";
 
 const Contact = () => {
   return (
-    <>
-      <Head>
-        <meta name="description" content="How to contact Emmanuel Jemeni" />
-        <title>Contact | Emmanuel Jemeni</title>
-      </Head>
-      <h1>Contact</h1>
-    </>
+    <PageTemplate
+      metaContent="How to contact Emmanuel Jemeni"
+      metaTitle="Contact | Emmanuel Jemeni"
+      pageSectionTitle="Contact"
+    >
+      <section>
+        <Quote
+          author="Garrison Keillor"
+          quote="Be well, do good work, and keep in touch."
+          cite_link="https://quotefancy.com/quote/1011786/Garrison-Keillor-Be-well-do-good-work-and-keep-in-touch"
+        />
+      </section>
+      {["Social Media", "Email", "Instant Messaging", "Communities"].map(
+        (category) => (
+          <section key={category} className="my-6">
+            <p className="text-left ml-[10%] text-5xl">{category}</p>
+            <div className="grid justify-items-center grid-cols-[repeat(auto-fit,_minmax(12rem,_1fr))] gap-x-4 gap-y-8 my-8 mx-0">
+              {contacts
+                .filter((contact) => contact.category === category)
+                .map((contact, index) => (
+                  <div
+                    key={`${index}__${contact.category}__${contact.name}`}
+                    className="flex items-center rounded py-6 px-3"
+                  >
+                    <div className="flex justify-center">
+                      <contact.image
+                        size="50%"
+                        title={contact.image_attribution || contact.name}
+                      />
+                    </div>
+                    <div className="text-4xl text-center">
+                      <p className=" underline-offset-[16px] transition-underline-offset ease-in-out duration-200 hover:underline hover:underline-offset-8">
+                        <a href={contact.url} target="_blank">
+                          {contact.name}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </section>
+        )
+      )}
+    </PageTemplate>
   );
 };
 
