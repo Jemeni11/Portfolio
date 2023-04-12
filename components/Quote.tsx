b99a8a1ab01f4f98c49fd65cae24d53abc4f89d1
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-12 17:21:07 +0100

    feat(Quote.tsx): Removed the react import. Added a *SrirachaClassName* import. Removed the placeholder code and completed the component
    BREAKING CHANGE:

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import SrirachaClassName from "@/components/FontsClassNames/SrirachaClassName";
 
 type QuotePropsType = {
   cite_link: string;
@@ -13,5 +13,20 @@ export default function Quote({
   author,
   work,
 }: QuotePropsType) {
-  return <div>Quote</div>;
+  return (
+    <figure className="w-4/5 bg-slate-500 min-h-max my-[20vh] mx-[10%] px-12 py-8 rounded-2xl text-center">
+      <blockquote
+        cite={cite_link}
+        className={
+          quote.length > 110 ? "text-[3.65rem] leading-none" : "text-6xl"
+        }
+      >
+        <p>"{quote}"</p>
+      </blockquote>
+      <figcaption className="mt-6 text-4xl">
+        —{author}
+        {work && <cite className={SrirachaClassName}>, {work}</cite>}
+      </figcaption>
+    </figure>
+  );
 }
