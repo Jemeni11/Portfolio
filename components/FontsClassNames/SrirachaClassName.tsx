976abea8d4e9bd85a2623be0783124cc2199fa01
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-12 17:15:38 +0100

    feat(FontsClassNames): Moved fonts to their own folder

---
+++ components/FontsClassNames/SrirachaClassName.tsx
@@ -0,0 +1,7 @@
+import { Sriracha } from "@next/font/google";
+
+const SrirachaFont = Sriracha({ weight: "400", subsets: ["latin"] });
+
+const SrirachaClassName = SrirachaFont.className;
+
+export default SrirachaClassName;
