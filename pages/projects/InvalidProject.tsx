43bd7c3ff51018eb46f3b660364dc043412a9172
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-12 17:28:11 +0100

    feat(Invalid*.tsx): Added new page components

---
+++ pages/projects/InvalidProject.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const InvalidProject = (props: Props) => {
+  return (
+    <div>InvalidProject</div>
+  )
+}
+
+export default InvalidProject
---
+++ pages/projects/InvalidUrl.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InvalidUrl = () => {
+  return <div>The url is invalid</div>;
+};
+
+export default InvalidUrl;
