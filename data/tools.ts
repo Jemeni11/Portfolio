02eb04a53eae4d12971e62ef9d803caf3d136a66
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-12 17:14:16 +0100

    feat(data/tools): Replaced tools.json with tools.ts

---
+++ data/tools.ts
@@ -0,0 +1,25 @@
+export const toolsArray = [
+  {
+    id: "hardware",
+    items: [
+      {
+        item_name: "mouse",
+        model: "2.4G Wireless Mouse Slim Rechargeable 3 Adjustable DPI",
+        links: {
+          jumia:
+            "https://www.jumia.com.ng/generic-led-2.4g-rechargeable-wireless-mouse-black-152871968.html",
+          amazon:
+            "https://www.amazon.com/Wireless-Rechargeable-Simplified-Adjustable-Chromebook/dp/B0B92TM7RS",
+        },
+      },
+      {
+        item_name: "earpiece",
+        model: "ORAIMO Conch OEP-E10",
+        links: {
+          jumia: "",
+          amazon: "",
+        },
+      },
+    ],
+  },
+];
