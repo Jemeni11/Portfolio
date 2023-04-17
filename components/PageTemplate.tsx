156414920b612b900b0320eb814658bcb6fc2431
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-17 17:43:58 +0100

    feat(PageTemplate.tsx): Removed the type definition and imported it from '@/types/page'

--- components/PageTemplate.tsx
+++ components/PageTemplate.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import FirstPageSection from "@/components/FirstPageSection";
+import type { PageTemplateType } from "@/types/page";
 
-type PageTemplateType = {
-  metaContent: string;
-  metaTitle: string;
-  pageSectionTitle: string;
-  children: React.ReactNode;
-};
-
+/**
+ * The PageTemplate component
+ * @param metaContent The content of the meta tag
+ * @param metaTitle The content of the title tag
+ * @param pageSectionTitle The title of the first page section
+ * @param children The children of the PageTemplate component
+ * */
 export default function PageTemplate({
   metaContent,
   metaTitle,
