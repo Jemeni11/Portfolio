b9b1e4095e932f3ff34665dfed565c961740a45a
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-04-17 17:45:35 +0100

    feat(HeroSection.tsx): Refactored HeroSection component to use SrirachaClassName instead of Sriracha font

--- components/HomePageSections/HeroSection.tsx
+++ components/HomePageSections/HeroSection.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
-import { Sriracha } from "@next/font/google";
+import SrirachaClassName from "@/components/FontsClassNames/SrirachaClassName";
 import CartoonMe from "@/public/cool_icon.png";
-
-const SrirachaFont = Sriracha({ weight: "400", subsets: ["latin"] });
 
 export default function HeroSection() {
   return (
     <div className="min-h-[85vh] w-full flex">
       <div className="flex-1 flex justify-center w-2/4 gap-[1.5rem] flex-col items-center">
         <h1 className="text-[3rem] font-extrabold">Hey, I'm Emmanuel</h1>
-        <p
-          className={`${SrirachaFont.className} text-[1.9rem] text-center px-12`}
-        >
+        <p className={`${SrirachaClassName} text-[1.9rem] text-center px-12`}>
           A Front-end Web Developer
         </p>
       </div>
