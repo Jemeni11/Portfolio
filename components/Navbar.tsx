77b08fd3f850d3c038f52875eddbba75ae403dac
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-07-02 21:36:56 +0100

    style: update Navbar component layout and navigation styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,39 +5,44 @@ import CartoonMe_NoBG from "../public/cartoon_me-removebg.png";
 export default function Navbar() {
   return (
     <nav className="flex justify-between w-full min-h-[13.75vh] py-[1.5vh] px-12">
-      <Link href="/">
+      <Link href="/" className="hidden lg:block">
         <div className="flex items-center">
           <Image
             src={CartoonMe_NoBG}
             alt="Icon"
             className="object-contain max-h-[10vh] max-w-[10%]"
           />
-          <div className="">
+          <div>
             <p className="text-xl font-bold">Emmanuel C. Jemeni</p>
             <p className="opacity-[.85]">@Jemeni11</p>
           </div>
         </div>
       </Link>
-      <ul className="flex-[2_2_0%] list-none flex gap-5 justify-end items-center text-2xl">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/tools">Tools</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/articles">Articles</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+      <ul className="flex-[2_2_0%] list-none flex gap-5 lg:justify-end justify-center items-center text-2xl">
+        {["", "about", "tools", "projects", "articles", "contact"].map(
+          (listItem) => {
+            return listItem === "" ? (
+              <NavListItem key={listItem + "_"} text="Home" href="/" />
+            ) : (
+              <NavListItem
+                key={listItem + "_"}
+                text={
+                  listItem.slice(0, 1).toLocaleUpperCase() + listItem.slice(1)
+                }
+                href={"/" + listItem}
+              />
+            );
+          }
+        )}
       </ul>
     </nav>
   );
 }
+
+const NavListItem = ({ href, text }: { href: string; text: string }) => {
+  return (
+    <li className="underline-offset-[16px] transition-underline-offset ease-in-out duration-200 hover:underline hover:underline-offset-8">
+      <Link href={href}>{text}</Link>
+    </li>
+  );
+};
