098b394bf438968112a6faee90692b04e4541991
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-07-02 21:41:44 +0100

    style: optimize Quote component layout

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,4 +1,4 @@
-import SrirachaClassName from "@/components/FontsClassNames/SrirachaClassName";
+import { SrirachaFont } from "@/pages/_app";
 
 type QuotePropsType = {
   cite_link: string;
@@ -14,19 +14,21 @@ export default function Quote({
   work,
 }: QuotePropsType) {
   return (
-    <figure className="w-4/5 bg-slate-500 min-h-max my-[20vh] mx-[10%] px-12 py-8 rounded-2xl text-center">
-      <blockquote
-        cite={cite_link}
-        className={
-          quote.length > 110 ? "text-[3.65rem] leading-none" : "text-6xl"
-        }
-      >
-        <p>"{quote}"</p>
-      </blockquote>
-      <figcaption className="mt-6 text-4xl">
-        —{author}
-        {work && <cite className={SrirachaClassName}>, {work}</cite>}
-      </figcaption>
-    </figure>
+    <div className="min-h-screen flex flex-col justify-center">
+      <figure className="w-4/5 bg-slate-500 min-h-max mx-[10%] px-12 py-8 rounded-2xl text-center">
+        <blockquote
+          cite={cite_link}
+          className={
+            quote.length > 110 ? "text-[3.65rem] leading-none" : "text-6xl"
+          }
+        >
+          <p>"{quote}"</p>
+        </blockquote>
+        <figcaption className="mt-6 text-4xl">
+          —{author}
+          {work && <cite className={SrirachaFont.className}>, {work}</cite>}
+        </figcaption>
+      </figure>
+    </div>
   );
 }
